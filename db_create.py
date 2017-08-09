f177168c934e38e0c874e24f4eb58f8f5990d04f
emolumberjack <jackmnewsom@gmail.com>
2017-08-08 22:38:32 -0700

    Changes to db_create

--- db_create.py
+++ db_create.py
@@ -6,11 +6,8 @@ import os.path
 
 db.create_all()
 
-try:
-	if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-	    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-	    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-	else:
-	    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
-except DatabaseAlreadyControlledError:
-	pass
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+	api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+	api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+	api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
