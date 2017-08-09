798f12213a9f7254c6af77de631857c6fc95483f
emolumberjack <jackmnewsom@gmail.com>
2017-08-08 22:57:44 -0700

    Changes to config.py

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 #location of database file
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 #location of folder for storing migration files
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
