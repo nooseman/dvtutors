4a2a1c45055fbe3605e3610cb6e4979c17704a72
emolumberjack <jackmnewsom@gmail.com>
2017-08-08 22:01:18 -0700

    Updated procfile

--- app/__init__.py
+++ app/__init__.py
@@ -33,12 +33,19 @@ from app.models import User
 def load_user(id):
 	return User.query.get(int(id))
 
-if not app.debug:
-	import logging
-	from logging.handlers import RotatingFileHandler
-	file_handler = RotatingFileHandler('tmp/tutor.log', 'a', 1 * 1024 * 1024, 10)
-	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-	app.logger.setLevel(logging.INFO)
-	file_handler.setLevel(logging.INFO)
-	app.logger.addHandler(file_handler)
-	app.logger.info('tutor startup')
+if not app.debug and os.environ.get('HEROKU') is None:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/tutor.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('tutor startup')
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('tutor startup')
