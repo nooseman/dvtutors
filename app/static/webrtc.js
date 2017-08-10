3957fd98f9615e0dfc453fb629a65cbd908e2534
emolumberjack <jackmnewsom@gmail.com>
2017-08-10 14:26:31 -0700

    added custom stun server

--- app/static/webrtc.js
+++ app/static/webrtc.js
@@ -1,6 +1,11 @@
+var stun = {
+	'url' : 'stun:stun.l.google.com:19302'
+};
+
 var webrtc = new SimpleWebRTC({
 	localVideoEl: 'localVideo',
 	remoteVideosEl: 'remotesVideos',
 	autoRequestMedia: true,
 	enableDataChannels: true
+	peerConnectionConfig: { 'iceServers': [stun, null]}
 });
