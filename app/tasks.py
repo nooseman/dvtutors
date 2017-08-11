9e5f8c138225f01f22214bb0e88cd6a1c89145a3
emolumberjack <jackmnewsom@gmail.com>
2017-08-11 13:44:47 -0700

    Changes to tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -3,7 +3,13 @@ from app import app, celery, db, models
 
 @celery.task()
 def remove_room(roomname):
-	models.Room.query.filter_by(roomname=roomname).approved_users = []
+	room = models.Room.query.filter_by(roomname=roomname)
+	room.approved_users = []
+
+	users = models.User.query.filter_by(room in approved_rooms).all()
+	for user in users:
+		user.approved_rooms.remove(room)
+
 	models.Room.query.filter_by(roomname=roomname).delete()
 	db.session.commit()
 
