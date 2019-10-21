ee1c8397a9cfb15095ec93e02429805b620d0fa0
Jack Newsom <jackmnewsom@gmail.com>
2019-10-21 15:51:58 -0700

    remove idiocy

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 WTF_CSRF_ENABLED = True
-SECRET_KEY = "Ya/g+!y3/s>~D[d)S67U4Ew/IiDaq+"
+SECRET_KEY = "boop"
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -16,12 +16,12 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 OAUTH_CREDENTIALS = {
 	'facebook': {
-		'id': '1703552289947767',
-		'secret': '709adbe160b1e033b437d84aed7d633b'
+		'id': 'eyed'
+		'secret': 'scoop'
 	},
 	'google': {
-		'id': '977228534153-8odev575d1qj301ebj0l01fe4jgk2t7a.apps.googleusercontent.com',
-		'secret': 'TxaIZ7riMFbVI5wxJP-ZiCBS'
+		'id': 'asdf'
+		'secret': 'scoopdiwoop'
 	}
 }
 
